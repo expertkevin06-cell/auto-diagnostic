a074035ec837104a3cc563efca39c8de4d5011a9
expertkevin06-cell <expertkevin06@gmail.com>
2026-08-31 15:26:08 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUSnO5Q_5M7JLN1vKo8ldJgvI9bM3caxM",
+  authDomain: "auto-diagnostic-dd7c2.firebaseapp.com",
+  databaseURL: "https://auto-diagnostic-dd7c2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "auto-diagnostic-dd7c2",
+  storageBucket: "auto-diagnostic-dd7c2.firebasestorage.app",
+  messagingSenderId: "80368231016",
+  appId: "1:80368231016:web:75f1785b49f2cc605811dd"
+};
